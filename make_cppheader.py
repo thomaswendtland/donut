80eb71222bbedbc2fa1e5dbf1c5e6d1e22c77197
twendtland@gmail.com <twendtland@gmail.com>
2017-07-17 15:24:30 +0200

    Type generation added

--- make_cppheader.py
+++ make_cppheader.py
@@ -24,6 +24,7 @@
 import svdparser
 import sys
 from collections import OrderedDict
+import fileinput
 
 # ------------------------------------------------------------------------------
 
@@ -33,9 +34,11 @@ FILE_HEADER = "\n#pragma once\n\n#include <cstdint>\n#include \"Bitfield.hpp\"\n
 PERIPHERAL_TEMPLATE_STRING = "\ttemplate <std::uint32_t BaseAddress, std::uint16_t Irq>\n"
 REGISTER_WIDTH_TYPE = "std::uint32_t"
 
+types = list()
+
 # strip digits at the end of a name
 # ------------------------------------------------------------------------------
-def normalize(name):
+def strip_trailing_digits(name):
     if name[0].isdigit():
         return name # return if all digits
     while name[len(name)-1].isdigit() == True:
@@ -77,11 +80,25 @@ def type_for_bitwidth(bitwidth):
 
 def write_field(register, field, header_file):
     field = standardize(field)
-    datatype = type_for_bitwidth(int(field["bitWidth"]))
+    datatype = ""
+    # add a global type this field
+    if "enumeratedValues" in field and len(field["enumeratedValues"]) > 1:
+        global types
+        datatype = field["name"].title() + "Type"
+        typestring = "\tenum class " + datatype + "{\n"
+        for value in  field["enumeratedValues"]:
+            # todo: remove leading digits
+            typestring += "\t\t" + value + ",\n"
+        typestring = typestring[:-2] + "\n" # delete comma for last item
+        typestring += "\t};\n"
+        types.append(typestring)
+    else:
+        datatype = type_for_bitwidth(int(field["bitWidth"]))
     if register["name"] == field["name"]:
         field["name"] = "Value"
 
     template_params = [register["name"].title(), datatype, field["bitOffset"], field["bitWidth"], field["access"]]
+
     header_file.write("\t\t\tusing " + field["name"].title() + " = Bitfield<" + ", ".join(template_params) + ">;\n")
 
 # ------------------------------------------------------------------------------
@@ -106,7 +123,7 @@ def write_register(register, header_file):
 
 def write_peripheral(peripheral, header_file):
     header_file.write(PERIPHERAL_TEMPLATE_STRING)
-    header_file.write("\tstruct " + normalize(peripheral["name"].title()) + "Controller" +" {\n")
+    header_file.write("\tstruct " + strip_trailing_digits(peripheral["name"].title()) + "Controller" +" {\n")
     for register in peripheral["registers"].items():
         write_register(register[1], header_file)
     header_file.write("\t};")
@@ -117,7 +134,7 @@ def write_instance(instance, header_file):
     interrupt = "0xFF"
     if "interrupt" in instance:
         interrupt = instance["interrupt"]["Value"]
-    header_file.write("\n\tusing " + instance["name"].title() + " = " + normalize(instance["name"].title()) + "Controller<" + instance["baseAddress"] + ", " + interrupt + ">;")
+    header_file.write("\n\tusing " + instance["name"].title() + " = " + strip_trailing_digits(instance["name"].title()) + "Controller<" + instance["baseAddress"] + ", " + interrupt + ">;")
 
 # ------------------------------------------------------------------------------
 
@@ -139,8 +156,8 @@ def main():
         print "Error: no such peripheral in the SVD provided"
         exit(-1)
 
-    header_file = open("header/" + device_name + "_" + normalize(peripheral_name.lower()) + ".hpp", "w")
-    header_file.write(FILE_HEADER + device_name + " {\n")
+    header_file = open("header/" + device_name + "_" + strip_trailing_digits(peripheral_name.lower()) + ".hpp", "w")
+    header_file.write(FILE_HEADER + device_name + " {\n\n   // Types\n\n")
 
     peripheral = peripherals[peripheral_name]
 
@@ -158,7 +175,13 @@ def main():
             write_instance(item[1], header_file)
 
     header_file.write("\n\n} // end of namespace " + device_name)
+
     header_file.close()
+
+    for line in fileinput.input(header_file.name, inplace=True):
+        if "Types" in line:
+            line = "\n".join(types)
+        print line,
 
     print "File successfully written to " + header_file.name
 
